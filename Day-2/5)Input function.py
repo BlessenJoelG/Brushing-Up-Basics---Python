5bc4a76ae4584b2c252787ef2f2348f56abea6aa
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-03 22:39:20 +0530

    new file

---
+++ Day-2/5)Input function.py
@@ -0,0 +1,10 @@
+# #basically in python programming input function is used to take input from the computer and then display it on the console
+# where as the input which is being taken is received or considered as a string by default 
+# & also in python iput function let's us display a message or prompt for entering the input
+# #syntax of input function
+# input("message or prompt")
+# #example of input function
+name = input("Enter your name:\n")
+age = int(input("Enter your age:\n"))
+print("hello my name is",name,"i'm",age,"years old")
+print("After 5 years you will be", age+5,"years old")
