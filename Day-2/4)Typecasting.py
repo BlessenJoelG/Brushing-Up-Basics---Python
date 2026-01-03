e436aa1faf3f5215192d36288afc67215ffb8e0c
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-03 12:05:39 +0530

    emmergency

---
+++ Day-2/4)Typecasting.py
@@ -0,0 +1,10 @@
+# Type casting in pyhton is basically changing the datatype of a variable to the other one 
+# their are 2 types of type casting in python they are
+"""1)Explicit conversion : programmer or user initiates to convert the datatype of the variable
+2)Implicit conversion : according to the higher and lower order of python conversion are made 
+through python from lower to higher levels of datatype"""
+a = "FALSE"
+b = "2"
+print(bool(a) + bool(b))
+print(int(a) + int(b))
+print(float(a) + float(b))
