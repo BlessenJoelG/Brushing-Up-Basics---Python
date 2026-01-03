0d3e71a9c879664299ffe8172c3ce1c2c8ec630b
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-03 11:40:38 +0530

    minor changes

--- Day-1/Modules.py
+++ Day-1/Modules.py
@@ -1,3 +1,7 @@
+"""Modules in python are a collection of program files that are created to be reused in programs rather than to write them again and again"""
+# # their are two types of Modules
+# 1)Built-in Modules : preintalled with the python software
+# 2)External Modules : user defined modules
 import numpy 
 import pandas
 import os
--- Day-1/Print function.py
+++ Day-1/Print function.py
@@ -1,6 +1,12 @@
+"""in Python programming print() is used to output the given values
+ whether they are strings,integers or any other datatypes"""
+# Syntax for Print() function: 
+# Print(<object>,sep=",",end = "\n", sys = stdout)
 print("Hello Blessen")
 print("Long time no see hah")
 print("This time pray to god ,ask him the power for knowledge and strength please be patient, please be strong, please fight for what you want")
 print("Hello", "Premik","how is life", "i think you are", 21, sep= "-")
 x = "premik"
 print(f"blessen is also known as {x}","he chose the name" ,"when he was in his 10 grade",sep ="\n")
+# format in python is used just like placing the values in print statements
+#  where as the difference is in format concept we can place the values enclosed in the "in" than placing them after ","
