61b816453c772ccc6bc9d66db18410197d7bd926
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-04 19:45:05 +0530

    adding first file of day 3

--- Day-3/String.py
+++ Day-3/String.py
@@ -1,1 +1,21 @@
-print("We are in Strings concept Now!!")
+x = "premik"
+print(x,"this is also a string enclosed in ''\n")
+
+#multiline string
+y = '''this is a multiline string which can 
+be created using 3 single quotes &
+ also get to know that strings can 
+ be enclosed in ' '," ", 3single quotes '''
+print(y)
+#accesing characters of a string 
+print("accesing characters of a string\n")
+print(x[0])
+print(x[1])
+print(x[2])
+print(x[3])
+print(x[4])
+print(x[5],"\n")
+#accesing characters using a for loop
+print("accesing characters using a for loop\n")
+for character in x:
+    print(character)
