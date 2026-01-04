c83db82c1dc9524006b6b3aa36c66f828bb35803
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-04 20:15:54 +0530

    wil be back after eating

---
+++ Day-3/String Slicing&-ve slicing.py
@@ -0,0 +1,16 @@
+""""in python range and indexing works like [inclusive:exclusive] 
+ where exclusive returns one less than the value placed
+negative slicing works from the end of the string where -1 
+ is the last character of the string"""
+
+x = "Premik learns Python again from scratch"
+print(x[0:7])
+print(x[14:20],"is created by guido van rossum")
+print(x[14:20],"is loved by",x[0:7])
+
+print("length function in python:\n uses 'len()' & returns a number")
+y = "Blessen"
+print("length of",y,"is",len(y))
+print("length of",x,"is",len(x))
+""""negative slicing works different it uses len of the string -the value placed"""
+
