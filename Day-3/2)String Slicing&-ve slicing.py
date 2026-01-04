ce8f205145dcfd7dfeaab6356bbc83ead69a2bed
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-04 23:09:48 +0530

    2 programs left for day 3

--- Day-3/2)String Slicing&-ve slicing.py
+++ Day-3/2)String Slicing&-ve slicing.py
@@ -4,6 +4,7 @@ negative slicing works from the end of the string where -1
  is the last character of the string"""
 
 x = "Premik learns Python again from scratch"
+print(x[0:])
 print(x[0:7])
 print(x[14:20],"is created by guido van rossum")
 print(x[14:20],"is loved by",x[0:7])
@@ -13,4 +14,9 @@ y = "Blessen"
 print("length of",y,"is",len(y))
 print("length of",x,"is",len(x))
 """"negative slicing works different it uses len of the string -the value placed"""
-
+z = "Sanjana"
+print("negative slicing of z[1:-3]:",z[1:-3])
+print("negative slicing of z[-3:-2]:",z[-3:-2])
+print("negative slicing of z[-1]:",z[:-1])
+print(y[1:-1])
+print(z[-1:])
