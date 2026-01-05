4ca148b0293441162225fe807ac089c59149e68a
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-05 10:39:43 +0530

    for_loop_file

---
+++ Day-4/2)for_loop.py
@@ -0,0 +1,31 @@
+#printing sequence of characters in a string through for loop
+# name = input("Enter your nickname:\n")
+# print(f"Your name is {name}")
+# for char in name:
+#     print(char)
+#accessing element in a list
+heroes = ["iron man", "black widow", "captain marvel", "thor", "dr-doom"]
+for hero in heroes:
+    print(hero)
+    for char in hero:
+        print(char)
+    print()
+#accessing elemts in tuple()
+friends = ("premik","joel","blessen","prem") 
+for drohi in friends:
+    print(drohi)  
+print()
+#rangefunction()
+# syntax: range(inclusve,exclusive,step)
+# iterating through a range of numbers
+print("The even numbers will be:")
+for i in range(0,10,2):
+    print(i)
+print()
+print("The odd numbers will be:")
+for i in range(1,10,2):
+    print(i)
+
+# print over 20000numbers
+for i in range(0,20001,1):
+    print(i)
