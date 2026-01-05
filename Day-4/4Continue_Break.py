97022d790df95398f12d104362a62631eb756979
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-05 13:08:21 +0530

    minor changes in continue and break program

--- Day-4/4Continue_Break.py
+++ Day-4/4Continue_Break.py
@@ -1,4 +1,4 @@
-# # break statement example:  the loop will terminate when it finds break statement
+# # # break statement example:  the loop will terminate when it finds break statement
 print("Break statement example\n")
 i = 0
 while(i < 12):
@@ -15,3 +15,8 @@ while(i<10):
         continue
     print("it's",i,"floor")
     i = i+1
+x =10
+if(x is True):
+    pass 
+print("see nothing happened") # pass is used when you want to have empty block of code  
+print("Actually their is a x with a value of 10")
