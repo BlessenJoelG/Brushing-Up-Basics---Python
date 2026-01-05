650f6428a84cbbe65221d80b5f5eadc33c596f92
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-05 10:01:00 +0530

    First file of the day

--- Day-4/Match case statements.py
+++ Day-4/Match case statements.py
@@ -1,1 +1,11 @@
-print("Hello match case see you today!")
+x = int(input("Enter your age:\n"))
+match x:
+    case x if x>0 and x<=18:
+        print("You are an teenager.")
+    case x if x>18 and x<=30:
+        print("You are a adult")
+    case x if x>30 and x<65:
+        print("you are a uncle")
+    case x:
+        print("You are a grandpa")
+    
