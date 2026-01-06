edfb02c86d44d4efcc59ed88417556b8234d7ad1
Blessen Joel Gorinkala <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-06 12:14:50 +0530

    Update 7)exercise_if_else.py

--- Day-3/7)exercise_if_else.py
+++ Day-3/7)exercise_if_else.py
@@ -4,9 +4,9 @@ print(times)
 timestamp = int(time.strftime('%H'))
 if timestamp <12:
     print(f"Good Morning Sir, it's {times} in the morning")
-elif timestamp >12 and timestamp < 16:
+elif timestamp >=12 and timestamp <16:
     print(f"Good afternoon Sir, it's {times} in the afternoon")
-elif timestamp >16 and timestamp < 20:
+elif timestamp >=16 and timestamp <20:
     print(f"Good Evening Sir, it's {times} in the evening")
 else:
     print(f"Good Night Sir, it's {times} in the night")
