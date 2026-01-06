a30b58445847d04cccdf0f609e5871496662899b
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-06 12:08:30 +0530

    just checking

---
+++ Day-5/3)list_metho.py
@@ -0,0 +1,16 @@
+# main and need to be known methods of list are 
+# 1.append
+# 2.sort()
+# 3.count()
+# 4.index()
+# 5.insert()
+# 6.extend()
+# 7.copy()
+# concatenate is an operation of list
+nums = [73,858,3,74,21,12,18,99,69]
+print(nums)
+#append()
+print("appending number 99",nums.append(5))
+colors = ["voilet", "indigo", "blue"]
+colors.append("green")
+print(colors)
