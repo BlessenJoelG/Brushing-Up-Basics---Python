1b3cb409a1f7a803b2cb30441675e26028f35161
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-07 11:38:40 +0530

    start of the day

--- Day-6/f strings.py
+++ Day-6/f strings.py
@@ -1,2 +1,13 @@
-print("Hello!! Day6\n")
-print("See You In Few Hours")
+mssg1 = "Hello {} ela unav ra"
+mssg2 = "edho mi daya lera {}"
+fact = "{1} and {0} are best friends"
+name1 = "Joel"
+name2 = "premu"
+print(mssg1.format(name1))
+print(mssg2.format(name2))
+print(fact.format(name1,name2))
+
+#this f string can also be writtened as:
+print(f"{name1} and {name2} are friends acting like enemies")
+flo_num = 69.69696969
+print(f" sexiest position in the world is {flo_num:.2f}")
