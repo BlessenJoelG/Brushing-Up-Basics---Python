96399694f5c9f67570352cdd8b816908ceabcc4e
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-08 11:00:12 +0530

    testing

---
+++ Day-6/hello.py
@@ -0,0 +1,7 @@
+print("Hello World")
+print("checking if github reverts the file that i deleted and committed")
+response = input("Enter what happened")
+if response == "YES":
+    print("priya it works priya it works")
+else:
+    print("Urkondi sharma ilantappudey gundeni raayi cheskovali")
