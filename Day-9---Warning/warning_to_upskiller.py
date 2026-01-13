80f90c35dce3cfeefd04796c6722bc56a2838c8c
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-13 23:33:57 +0530

    warning message to the upscaler

---
+++ Day-9---Warning/warning_to_upskiller.py
@@ -0,0 +1,8 @@
+name = input("Enter your name here:\n")
+hardwork = input("Enter 'Y' if you completed ATS of Resume with 90+ & applied for Deloite company or else enter 'N':\n")
+if(hardwork == 'Y'):
+    print(f"{name}, Your Patience & Hardwork payed off i'm proud of you")
+elif(hardwork == 'N'):
+    print(f"{name}, Don't be demotivated be happy for want you have learned from this event and upscale again n again")
+else:
+    print(f"{name},Be patient and consistent, even if you don't get it you will soon be figure it out amen")
