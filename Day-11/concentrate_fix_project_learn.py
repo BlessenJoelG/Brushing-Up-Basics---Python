5121784715b8d86d142d49d3bab3397a7a277183
BlessenJoelG <154801802+BlessenJoelG@users.noreply.github.com>
2026-01-20 13:45:20 +0530

    restart-12 fail quote

---
+++ Day-11/concentrate_fix_project_learn.py
@@ -0,0 +1,8 @@
+print("currently learn python and sql only and build projects even simple one with the amount of concept you learn")
+word = input("enter 'Y' or 'N':\n")
+match word:
+    case 'Y':
+        print("You are on the right track")
+    case 'N':
+        print("You need to change your mindset")
+# select Discipline from mindset;
